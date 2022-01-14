6c64a470c9daddf32da99bf6815051caa60fe7f9
mikolaj.duszyk <mikolaj.duszyk@MIKOLAJ>
2022-01-14 20:21:21 +0100

    Dodaj pliki projektów.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+
+    public class FormDataModel
+    {
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+        [Display(Name = "Ilość powtórzeń")]
+        public int RepetitionCount { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FormDataModel model)
+        {
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
